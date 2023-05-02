e45369195cf6eac0bdb9904ac157488bb2a60b7b
Maria <122727669+MariaFeli@users.noreply.github.com>
2023-05-02 15:49:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { formik, Field, Form } from 'formik';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const Basic = () => (
-  <div>
-    <h1>Sign Up</h1>
-    <formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={async (values) => {
-        await new Promise((r) => setTimeout(r, 500));
-        alert(JSON.stringify(values, null, 2));
-      }}
-    >
-      <Form>
-        <label htmlFor="firstName">First Name</label>
-        <Field id="firstName" name="firstName" placeholder="Jane" />
-
-        <label htmlFor="lastName">Last Name</label>
-        <Field id="lastName" name="lastName" placeholder="Doe" />
-
-        <label htmlFor="email">Email</label>
-        <Field
-          id="email"
-          name="email"
-          placeholder="jane@acme.com"
-          type="email"
-        />
-        <button type="submit">Submit</button>
-      </Form>
-    </formik>
-  </div>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(<Basic />, document.getElementById('root'));
